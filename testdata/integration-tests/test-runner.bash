f661054e8b71f6b939dbc89d0a1ab81ec86caf44
Sebastian Radloff <sradloff23@gmail.com>
2020-04-24 17:16:18 -0400

    Adds basic integration testing to ensure tf file is created properly and can work properly with the terraform-provider-k8s

--- testdata/integration-tests/test-runner.bash
+++ testdata/integration-tests/test-runner.bash
@@ -2,12 +2,15 @@
 
 set -eu pipefail
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 kind_cluster="integration-tests"
 
 
 if [[ $(kind get clusters | grep "${kind_cluster}") == "" ]]; then
     echo "Creating kind cluster ${kind_cluster}"
-    kind create cluster --image kindest/node:v1.17.0 --name "${kind_cluster}" --config kind.yaml
+    kind create cluster --image kindest/node:v1.18.0 --name "${kind_cluster}" --config kind.yaml
 else
     echo "Using kind cluster ${kind_cluster} that already exists"
 fi
@@ -18,8 +21,41 @@ kind export kubeconfig --name "${kind_cluster}"
 MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${kind_cluster}"-control-plane)
 sed -i "s/server:.*/server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/config
 
+
+set -x
+k8s_file_dir="../k8s-files"
+cd "${DIR}"
+
+rawk8stfc inline -f "${k8s_file_dir}/one-obj.yaml" -o test-1.tf
+
 terraform init
-terraform plan
-terraform destroy -auto-approve
 terraform apply -auto-approve
-# terraform destroy -auto-approve
+sleep 3
+terraform destroy -auto-approve
+rm test-1.tf
+
+
+rawk8stfc inline -f "${k8s_file_dir}/multiple-objs.yaml" -o test-2.tf
+
+terraform init
+terraform apply -auto-approve
+sleep 3
+terraform destroy -auto-approve
+rm test-2.tf
+
+rawk8stfc file-reference -f "${k8s_file_dir}/one-obj.yaml" -o test-3.tf
+
+terraform init
+terraform apply -auto-approve
+sleep 3
+terraform destroy -auto-approve
+rm test-3.tf
+
+
+rawk8stfc file-reference -f "${k8s_file_dir}/multiple-objs.yaml" -o test-4.tf
+
+terraform init
+terraform apply -auto-approve
+sleep 3
+terraform destroy -auto-approve
+rm test-4.tf
